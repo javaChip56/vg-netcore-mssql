fc968977bb60ad51e99d877ab5b5b03a8dafae79
Alejandro Manadong Gali <gali@pspl.phillip.com>
2019-11-15 16:42:41 +0800

    Added basic GET endpoints.

--- api/ClientAPI/Controllers/ClientController.cs
+++ api/ClientAPI/Controllers/ClientController.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
+using ClientAPI.Domain.Models;
+using ClientAPI.Domain.Queries.Interfaces;
+
 namespace ClientAPI.Controllers
 {
     // [Route("api/[controller]")]
@@ -12,7 +15,34 @@ namespace ClientAPI.Controllers
     [ApiController]
     public class ClientController : ControllerBase
     {
+        private IClientQueries _clientQueries;
+        public ClientController(IClientQueries clientQueries)
+        {
+            this._clientQueries = clientQueries;
+        }
 
+        [HttpGet]
+        public async Task<Client> GetClient(int id)
+        {
+            return await this._clientQueries.GetClientAsync(id);
+        }
+
+        [HttpGet]
+        public async Task<Client> GetClientByNo(string clientNo)
+        {
+            return await this._clientQueries.GetClientAsync(clientNo);
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Client>> ListAll()
+        {
+            return await this._clientQueries.GetAllClientsAsync();
+        }
         
+        [HttpGet]
+        public async Task<IEnumerable<ClientAccount>> GetClientAccounts(string clientNo)
+        {
+            return await this._clientQueries.GetClientAccountsAsync(clientNo);
+        }
     }
 }
--- api/ClientAPI/Startup.cs
+++ api/ClientAPI/Startup.cs
@@ -63,10 +63,25 @@ namespace ClientAPI
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseHttpsRedirection();
             }
 
-            app.UseHttpsRedirection();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint(
+                    string.Format(
+                        Configuration["SwaggerConfig:Endpoint"],
+                        Configuration["SwaggerConfig:FriendlyName"] + Configuration["SwaggerConfig:Version"]), 
+                        Configuration["SwaggerConfig:Title"] + " " + Configuration["SwaggerConfig:Version"]);
+            });
+
+            app.UseHealthChecks(path: "/hc", new HealthCheckOptions() {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
             app.UseMvc();
+
         }
     }
 }
